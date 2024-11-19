623ae4af880907ac26861ce343f45077f4eb5e2e
Luis Galvan <67127384+luis920@users.noreply.github.com>
2024-11-19 13:54:23 +0000

    proyecto terminado

--- src/js/component/CardContactos.jsx
+++ src/js/component/CardContactos.jsx
@@ -8,50 +8,57 @@ export const Card = () => {
 
   useEffect(() => {
     actions.getContacts();
-  }, []);
+  }, [actions]);
+
+  if (store.contactos.length === 0) {
+    return (
+      <div className="container text-center">
+        <h1 className="text-light">Actualmente no existen contactos.</h1>
+      </div>
+    );
+  }
+
   
-
-
   return (
     <>
       {store.contactos.map((item, index) => {
         return (
-          <div className="card mx-auto" key={index}>
+          <div className="card mx-auto bg-dark text-light" key={index}>
             <div className="row g-0">
               <div className="col-md-4">
                 <img
-                  src="https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"
+                  src="https://picsum.photos/200/300"
                   className="img-card"
                   alt="..."
                 />
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{item.name}</h5>
-                  <p className="card-text">Address: {item.address}</p>
-                  <p className="card-text">Phone: {item.phone}</p>
-                  <p className="card-text">Email: {item.email}</p>
+                  <h3 className="card-title">{item.name}</h3>
+                  <p className="card-text"><strong>Address:</strong>  {item.address}</p>
+                  <p className="card-text"><strong>Phone:</strong> {item.phone}</p>
+                  <p className="card-text"><strong>Email:</strong> {item.email}</p>
                 </div>
                 <div className="iconos">
-  <button
-    className="btn btn-primary btn-sm w-40"
-    onClick={() => {
-      if (window.confirm("¿Estás seguro de que deseas eliminar este contacto?")) {
-        actions.removeContact(item.id);
-      }
-    }}
-  >
-    <i className="fas fa-trash"></i>
-  </button>
+                  <button
+                    className="btn btn-primary btn-sm w-40"
+                    onClick={() => {
+                      if (window.confirm("¿Estás seguro de que deseas eliminar este contacto?")) {
+                        actions.removeContact(item.id);
+                      }
+                    }}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </button>
 
-  <Link
-    to={`/edit/${item.id}`}
-    className="btn btn-primary btn-sm w-40"
-    aria-label="Edit contact"
-  >
-    <i className="fa-solid fa-pen-to-square"></i>
-  </Link>
-</div>
+                  <Link
+                    to={`/edit/${item.id}`}
+                    className="btn btn-primary btn-sm w-40"
+                    aria-label="Edit contact"
+                  >
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar  mb-3">
 			<Link to="/">
 				
 			</Link>
-			<div className="ml-auto">
+			<div >
 				<Link to="/formContact">
-					<button className="btn btn-primary">Agregar contacto</button>
+					<button className="btn btn-primary ">Agregar contacto</button>
 				</Link>
 			</div>
 		</nav>
